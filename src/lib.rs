89a13e9b3d3eccc3a6f79c5d90f5f3a9118ed0fa
Lakshya Singh <lakshay.singh1108@gmail.com>
2024-11-23 15:27:54 +0530

    use buf reader with hashmap
    Signed-off-by: Lakshya Singh <lakshay.singh1108@gmail.com>

--- src/lib.rs
+++ src/lib.rs
@@ -1,90 +1,62 @@
-use std::{collections::BTreeMap, path::Path};
+use std::{
+    collections::HashMap, fs::File, io::{BufRead, BufReader}, path::Path
+};
 
-pub fn add(left: u64, right: u64) -> u64 {
-    left + right
+pub struct CityState {
+    pub min: f32,
+    pub max: f32,
+    sum: f32,
+    count: usize,
 }
 
-pub struct TemperatureRecording {
-    pub city: String,
-    pub value: f32,
-}
-
-impl TemperatureRecording {
-    fn new(city_name: String, temperature: f32) -> Self {
-        TemperatureRecording {
-            city: city_name,
-            value: temperature,
-        }
-    }
-}
-
-pub struct CityRecording {
-    pub city: String,
-    pub readings: Vec<f32>,
-}
-
-impl CityRecording {
-    fn new(city_name: String, readings: Vec<f32>) -> Self {
-        let mut readings = readings;
-        readings.sort_by(|a, b| a.partial_cmp(b).expect("uncomparable floats"));
-        CityRecording {
-            city: city_name,
-            readings,
+impl CityState {
+    pub fn new(initial_recording: f32) -> Self {
+        CityState {
+            min: initial_recording,
+            max: initial_recording,
+            sum: initial_recording,
+            count: 1,
         }
     }
 
-    pub fn min(&self) -> f32 {
-        *self.readings.get(0).expect("empty readings")
+    pub fn mean(&self) -> f32 {
+      return self.sum / (self.count as f32)
     }
 
-    pub fn max(&self) -> f32 {
-        *self
-            .readings
-            .get(self.readings.len() - 1)
-            .expect("empty readings")
+    pub fn update(&mut self, recording: f32) {
+      self.min = self.min.min(recording);
+      self.max = self.max.max(recording);
+      self.sum += recording;
+      self.count += 1;
     }
+}
 
-    pub fn avg(&self) -> f32 {
-        if self.readings.len() == 0 {
-            return 0f32;
+pub fn create_buffered_reader(path: &Path) -> BufReader<File> {
+    let file = File::open(path).expect("file open");
+    BufReader::new(file)
+}
+
+pub fn read_file(path: &Path) -> HashMap<String, CityState> {
+    let mut reader = create_buffered_reader(path);
+    let mut current_line = String::with_capacity(200); // 2 bytes * 100 characters UTF-8
+    let mut map = HashMap::new();
+    loop {
+        let bytes_read = reader.read_line(&mut current_line).expect("read line");
+        if bytes_read == 0 {
+            break;
         }
+        let record: Vec<&str> = current_line.splitn(2, ';').collect();
+        let city_name = record.get(0).expect("city name missing").to_string();
+        let temperature: f32 = record
+            .get(1)
+            .expect("reading missing")
+            .trim()
+            .parse()
+            .expect("invalid float");
 
-        self.readings.iter().sum::<f32>() / self.readings.len() as f32
+        map.entry(city_name).or_insert_with(|| CityState::new(temperature)).update(temperature);
+
+        current_line.clear();
     }
-}
-
-pub fn read_file(path: &Path) -> Vec<TemperatureRecording> {
-    let file_content = std::fs::read_to_string(path).expect("file read failed");
-    let file_lines: Vec<&str> = file_content.split('\n').collect();
-    file_lines
-        .iter()
-        .map(|&line| line.trim())
-        .filter(|&line| !line.is_empty())
-        .map(|line| {
-            let record: Vec<&str> = line.splitn(2, ';').collect();
-            // println!("{:?}", record);
-            let city_name = record.get(0).expect("city name missing").to_string();
-            let temperature = record
-                .get(1)
-                .expect("reading missing")
-                .parse()
-                .expect("invaluid float");
-            TemperatureRecording::new(city_name, temperature)
-        })
-        .collect()
-}
-
-pub fn cluster_values(temperature_recordings: &Vec<TemperatureRecording>) -> Vec<CityRecording> {
-    let mut map = BTreeMap::new();
-    temperature_recordings.iter().for_each(|reading| {
-        map.entry(reading.city.clone())
-            .or_insert_with(Vec::new)
-            .push(reading.value)
-    });
-
-    // map.iter().for_each(|(key, value)| println!("{}: {:?}", key, value));
-
-    map.iter()
-        .map(|(key, values)| CityRecording::new(key.clone(), values.clone()))
-        .collect()
+    map
 }
--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,12 @@
-use std::path::Path;
 use abrc::*;
+use std::path::Path;
+
 fn main() {
-  let path = Path::new("measurements.txt");
-  let file_content = read_file(path);
-  let city_recordings = cluster_values(&file_content);
-  let results: Vec<String> = city_recordings
-    .iter()
-    .map(|city| format!("{}={:?}/{}/{:?}", city.city, city.min(), city.avg(), city.max()))
-      .collect();
-  println!("{{{}}}", results.join(", "))
+    let path = Path::new("measurements.txt");
+    let city_state = read_file(path);
+    let results: Vec<String> = city_state
+        .iter()
+        .map(|(city_name, city)| format!("{}={}", city_name, city))
+        .collect();
+    println!("{{{}}}", results.join(", "))
 }
